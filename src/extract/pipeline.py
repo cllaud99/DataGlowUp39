736feb9a83fcef364d6f6f11efcf7f7987ebc0ec
cllaud99 <claudiohpontes@gmail.com>
2024-12-23 18:00:59 -0300

    Adicionada pipeline simplificada

--- src/extract/pipeline.py
+++ src/extract/pipeline.py
@@ -1,4 +1,4 @@
-from download_files import listar_links_zip_rar, baixar_arquivos
+from download_files import main_download
 from extract_file import extrair_arquivos_zip
 
 
@@ -7,13 +7,7 @@ def main():
     pasta_destino = "downloads"
     pasta_extraida = "downloads/extraidos"
 
-    url = 'http://dados.prefeitura.sp.gov.br/it/dataset/microdados-matriculas'
-    links = listar_links_zip_rar(url)
-
-    baixar_arquivos(links, pasta_destino=pasta_destino)
-
-    links = listar_links_zip_rar(url)
-    baixar_arquivos(links)
+    main_download()
 
     extrair_arquivos_zip(pasta_destino, pasta_extraida)
 
